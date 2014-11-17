72e87169fea78aec63b021278fc3ac39f66c9c8e
xthr3mx <xthr3mx@gmail.com>
2014-11-16 21:31:05 -0600

    Integracion de bootstrap y de la logica para registrar personas.

---
+++ resources/js/main.js
@@ -0,0 +1,50 @@
+$(document).ready(start);
+
+var data = {};
+
+function getDataFromHTMLForm(){
+	data.nombre = $("#nombre").val();
+	data.email = $("#email").val();
+	data.comentario = $("#comentario").val();
+};
+
+function displayMessage(json){
+	if(!json.error_status){
+		$("#d-success").css("display","block");
+		$("#d-success p.message").text(json.message);
+	}else{
+		$("#d-error").css("display","block");
+		$("#d-error p.error_description").text(json.message);
+	}
+};
+
+function clearInformationFromHTMLForm(){
+	$("#nombre").val("");
+	$("#email").val("");
+	$("#comentario").val("");
+	$("#nombre").focus();
+};
+
+function sendDataToServer(){
+	$.ajax({
+		type: "POST",
+		url: 'resources/php/index.php',
+		data: data,
+		success: function(json_response,textStatus,jqXHR){
+			var json = JSON.parse(json_response);
+			displayMessage(json);
+			clearInformationFromHTMLForm();
+		},
+		error: function(jqXHR, textStatus, errorThrown){
+
+		}
+	});
+};
+
+function start(){
+	$("#contact-form").submit(function(event){
+		getDataFromHTMLForm();
+		sendDataToServer();
+		event.preventDefault();
+	});
+};
---
+++ resources/php/index.php
@@ -0,0 +1,55 @@
+<?php 
+
+	$db_host = "127.0.0.1";
+	$db_username = "d3m0";
+	$db_password = 'd3_m05$eR.';
+	$db_name = "agenda";
+	$connection = null;
+	$data = array();
+
+	try{
+		$connection = new mysqli($db_host,$db_username,$db_password,$db_name);
+
+		// check connection
+		if($connection->connect_error){
+			$data["error_status"] = true;
+			$data["error_description"] = "Ocurrio un problema al intentar registrar. Por favor intentarlo mas tarde o contacte al administrador.";	
+			trigger_error('Database connection failed: '+$connection->connect_error, E_USER_ERROR);
+		}
+
+		if(!empty($_POST)){
+			$nombre = $_POST['nombre'];
+			$email = $_POST['email'];
+			$direccion = $_POST['direccion'];
+			$sql_command = "INSERT INTO persona (nombre,email,direccion) VALUES ('$nombre', '$email', '$direccion')";
+
+			if ($connection->query($sql_command)===false){
+				$data["error_status"] = true;
+				$data["error_description"] = "Problemas al intentar consultar en la base de datos.";
+				trigger_error('Wrong SQL: '.$sql_command.' Error: '.$connection->error,E_USER_ERROR);
+			}else{
+				//$last_inserted_id = $connection->insert_id;
+				$affected_rows = $connection->affected_rows;
+				if($affected_rows==1){
+					$data["error_status"] = false;
+					$data["error_description"] = "";
+					$data["message"] = "La persona ha sido registrada. Muchas gracias por registrase.";
+				}else{
+					$data["error_status"] = false;
+					$data["error_description"] = "";
+					$data["message"] = "Ocurrio un problema al intentar registrar. Por favor intentarlo mas tarde o contacte al administrador.";
+				}
+			}
+
+		}
+
+		$connection = null;
+
+	}catch(Exception $exception){
+		echo $exception;
+	}
+	
+echo json_encode($data);
+
+?>
+
